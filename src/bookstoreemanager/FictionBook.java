e57b6cd00ebba061a0a17989583b2667e1fa5831
jana Alsinani <125142926+Jana1e@users.noreply.github.com>
2024-04-20 16:55:38 +0300

    Update FictionBook.java

--- src/bookstoreemanager/FictionBook.java
+++ src/bookstoreemanager/FictionBook.java
@@ -1,8 +1,29 @@
-package BookstoreManager;
+package BookstoreeManager;
 
-public class FictionBook extends Book {
+public class FictionBook implements Book {
+    private String title;
+    private Author author;
+    private double price;
+
     public FictionBook(String title, Author author, double price) {
-        super(title, author, price);
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public Author getAuthor() {
+        return author;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
     }
 
     @Override
