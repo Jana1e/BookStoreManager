e16b49d9cdd1e7af10c85afa770c79b2e532d17f
jana Alsinani <125142926+Jana1e@users.noreply.github.com>
2024-04-20 16:55:09 +0300

    Update Book.java

--- src/bookstoreemanager/Book.java
+++ src/bookstoreemanager/Book.java
@@ -1,28 +1,8 @@
-package BookstoreManager;
+package BookstoreeManager;
 
-public abstract class Book implements BookType {
-    private String title;
-    private Author author;
-    private double price;
-
-    public Book(String title, Author author, double price) {
-        this.title = title;
-        this.author = author;
-        this.price = price;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    // Factory Method
-    public abstract String getBookType();
+public interface Book {
+    String getTitle();
+    Author getAuthor();
+    double getPrice();
+    String getBookType();
 }
