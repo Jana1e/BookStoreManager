6ebb32e1c48e4a0d8710e5dbadc5cdf1ced9402f
jana Alsinani <125142926+Jana1e@users.noreply.github.com>
2024-05-14 00:33:28 +0300

    Update InventoryManager.java with the Observer Pattern

--- src/bookstoreemanager/InventoryManager.java
+++ src/bookstoreemanager/InventoryManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class InventoryManager {
     private static InventoryManager instance;
     private List<Book> books;
+    private List<InventoryObserver> observers = new ArrayList<>();
 
     private InventoryManager() {
         this.books = new ArrayList<>();
@@ -17,16 +18,34 @@ public class InventoryManager {
         return instance;
     }
 
+    public void addObserver(InventoryObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(InventoryObserver observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(Book book, String action) {
+        for (InventoryObserver observer : observers) {
+            observer.update(book, action);
+        }
+    }
+
     public void addBook(Book book) {
         books.add(book);
+        notifyObservers(book, "added");
     }
 
     public void removeBook(Book book) {
         books.remove(book);
+        notifyObservers(book, "removed");
     }
+
 
     public List<Book> getBooks() {
         return books;
     }
+
     
 }
