668bbac364bd0162def88cf8e80664ba82f8b7a4
jana Alsinani <125142926+Jana1e@users.noreply.github.com>
2024-05-14 00:34:48 +0300

    Update SalesManager.java with the Observer Pattern

--- src/bookstoreemanager/SalesManager.java
+++ src/bookstoreemanager/SalesManager.java
@@ -1,19 +1,28 @@
-package BookstoreManager;
+package bookstoremanager;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class SalesManager {
+public class SalesManager implements InventoryObserver {
     private List<Order> orders;
 
     public SalesManager() {
         this.orders = new ArrayList<>();
     }
-
+    @Override
+    public void update(Book book, String action) {
+        System.out.println("Sales Manager notified about book: " + book.getTitle() + ", Action: " + action);
+    }
     public void processOrder(Order order) {
         orders.add(order);
     }
 
+    // Getters and setters
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 }
