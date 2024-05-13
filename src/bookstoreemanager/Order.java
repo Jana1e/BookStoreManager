12b66986de960c2a7bd2ec4dd5b636413d390b12
jana Alsinani <125142926+Jana1e@users.noreply.github.com>
2024-05-14 00:39:04 +0300

    Update Order.java with the Strategy Pattern

--- src/bookstoreemanager/Order.java
+++ src/bookstoreemanager/Order.java
@@ -1,16 +1,23 @@
-package BookstoreManager;
-import java.util.ArrayList;
-import java.util.List;
+package bookstoremanager;
+
+import java.util.*;
 
 public class Order {
     private Customer customer;
     private List<Book> books;
     private double totalPrice;
+    private PricingStrategy pricingStrategy;
 
-    public Order(Customer customer) {
+    
+     public Order(Customer customer, PricingStrategy strategy) {
         this.customer = customer;
         this.books = new ArrayList<>();
         this.totalPrice = 0.0;
+        this.pricingStrategy = strategy;
+    }
+
+    public void setPricingStrategy(PricingStrategy strategy) {
+        this.pricingStrategy = strategy;
     }
 
     public void addBook(Book book) {
@@ -18,17 +25,34 @@ public class Order {
         totalPrice += book.getPrice();
     }
 
+    // Getters and setters
     public Customer getCustomer() {
         return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 
     public List<Book> getBooks() {
         return books;
     }
 
-    public double getTotalPrice() {
-        return totalPrice;
+    public void setBooks(List<Book> books) {
+        this.books = books;
     }
 
-}
    
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    
+    public double getTotalPrice() {
+        double totalPrice = 0.0;
+        for (Book book : books) {
+            double bookPrice = pricingStrategy.calculatePrice(book);
+            totalPrice += bookPrice;
+        }
+        return totalPrice;
+    }
